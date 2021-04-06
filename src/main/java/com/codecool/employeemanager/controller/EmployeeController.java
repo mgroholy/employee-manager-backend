cb3bbed6192e0e0117a522fd54466d38f60c5477
Kriszti Toth <k95toth@gmail.com>
2021-04-06 17:00:56 +0200

    fix: add CrossOrigin annotation to EmployeeController

--- src/main/java/com/codecool/employeemanager/controller/EmployeeController.java
+++ src/main/java/com/codecool/employeemanager/controller/EmployeeController.java
@@ -3,11 +3,13 @@ package com.codecool.employeemanager.controller;
 import com.codecool.employeemanager.model.Employee;
 import com.codecool.employeemanager.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class EmployeeController {
 
