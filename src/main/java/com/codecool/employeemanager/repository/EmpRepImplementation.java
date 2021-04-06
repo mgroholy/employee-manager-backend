6347d7b0d71947640e462c6d17ef4ec65245a98d
Kriszti Toth <k95toth@gmail.com>
2021-04-06 15:47:42 +0200

    feat: define EmployeeRepository interface

--- src/main/java/com/codecool/employeemanager/repository/EmpRepImplementation.java
+++ src/main/java/com/codecool/employeemanager/repository/EmpRepImplementation.java
@@ -1,7 +1,53 @@
 package com.codecool.employeemanager.repository;
 
+import com.codecool.employeemanager.model.Employee;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class EmpRepImplementation implements EmployeeRepository{
+
+    private List<Employee> employees = new ArrayList<>();
+
+    @Override
+    public void save(Employee employee) {
+
+    }
+
+    @Override
+    public void update(Employee employee) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Employee findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> findByDepartment(String departmentName) {
+        return null;
+    }
 }
--- src/main/java/com/codecool/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/com/codecool/employeemanager/repository/EmployeeRepository.java
@@ -1,4 +1,18 @@
 package com.codecool.employeemanager.repository;
 
+import com.codecool.employeemanager.model.Employee;
+
+import java.util.List;
+
 public interface EmployeeRepository {
+
+    void save(Employee employee);
+    void update(Employee employee);
+    void deleteById(int id);
+    List<Employee> findAll();
+    Employee findById(int id);
+    Employee findByEmail(String email);
+    List<Employee> findByName(String name);
+    List<Employee> findByDepartment(String departmentName);
+
 }
