f92bc39276d08c6f5809b54d4756d1533626e52c
Kriszti Toth <k95toth@gmail.com>
2021-04-06 16:35:20 +0200

    feat: implement json response for list of employees

--- src/main/java/com/codecool/employeemanager/controller/EmployeeController.java
+++ src/main/java/com/codecool/employeemanager/controller/EmployeeController.java
@@ -1,7 +1,27 @@
 package com.codecool.employeemanager.controller;
 
+import com.codecool.employeemanager.model.Employee;
+import com.codecool.employeemanager.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/")
+    public List<Employee> getAllEmployees() {
+        return employeeService.findAllEmployees();
+    }
+
+
 }
--- src/main/java/com/codecool/employeemanager/service/EmployeeService.java
+++ src/main/java/com/codecool/employeemanager/service/EmployeeService.java
@@ -1,7 +1,27 @@
 package com.codecool.employeemanager.service;
 
+import com.codecool.employeemanager.model.Employee;
+import com.codecool.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
 }
