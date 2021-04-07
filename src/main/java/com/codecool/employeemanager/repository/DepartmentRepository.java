f18b9e6e8a389cf86181181d9cc8e8ff660a6d0b
Máté Groholy <mate.groholy@gmail.com>
2021-04-07 21:36:31 +0200

    feat: add findByName definition to DepartmentREpository.

--- src/main/java/com/codecool/employeemanager/repository/DepartmentRepository.java
+++ src/main/java/com/codecool/employeemanager/repository/DepartmentRepository.java
@@ -3,10 +3,12 @@ package com.codecool.employeemanager.repository;
 import com.codecool.employeemanager.model.Department;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DepartmentRepository {
 
     void save(Department department);
+    Optional<Department> findByName(String departmentName);
     List<Department> findAll();
 
 }
