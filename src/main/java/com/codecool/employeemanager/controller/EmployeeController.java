e80616b71304df1b010a01869272a9fe558aab12
Kriszti Toth <k95toth@gmail.com>
2021-04-07 16:54:02 +0200

    feat: implement employee removal by id

--- src/main/java/com/codecool/employeemanager/controller/EmployeeController.java
+++ src/main/java/com/codecool/employeemanager/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @CrossOrigin
@@ -32,8 +33,13 @@ public class EmployeeController {
         return employeeService.addEmployee(employee);
     }
 
-    @GetMapping("/employees/{id}")
-    public Employee getEmployeeById(@PathVariable int id) {
-        return employeeService.findEmployeeById(id);
+    @RequestMapping(value = "/employees/{id}", method = { RequestMethod.GET, RequestMethod.DELETE })
+    public Employee getEmployeeById(@PathVariable int id, HttpServletRequest request) {
+        if (request.getMethod().equals("GET")) {
+            return employeeService.findEmployeeById(id);
+        } else {
+            employeeService.deleteEmployeeById(id);
+            return null;
+        }
     }
 }
