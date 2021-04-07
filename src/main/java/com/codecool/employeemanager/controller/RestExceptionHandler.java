dbdb513e263f25df54d2ca5d0c958c9b43849b3e
Máté Groholy <mate.groholy@gmail.com>
2021-04-07 11:18:41 +0200

    refactor: remove unused LocalDate import from RestExceptionHandler.

--- src/main/java/com/codecool/employeemanager/controller/RestExceptionHandler.java
+++ src/main/java/com/codecool/employeemanager/controller/RestExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 
