bc296dbce2e3b6befcf0d56c442ff313886ab24d
Máté Groholy <mate.groholy@gmail.com>
2021-04-19 14:00:30 +0200

    feat: use Lombok in models.

--- src/main/java/com/codecool/employeemanager/model/Department.java
+++ src/main/java/com/codecool/employeemanager/model/Department.java
@@ -1,39 +1,20 @@
 package com.codecool.employeemanager.model;
 
+import lombok.*;
+
 import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
 public class Department {
 
     private int id;
+    @NonNull
     private String name;
 
-    public Department(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Department(String name) {
-        this.name = name;
-    }
-
-    public Department() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public boolean equals(Object o) {
