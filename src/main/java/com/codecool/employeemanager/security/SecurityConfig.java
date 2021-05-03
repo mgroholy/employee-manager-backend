2e673771c03703277aae7ad7f51fd5362771d511
Máté Groholy <mate.groholy@gmail.com>
2021-05-03 15:05:01 +0200

    feat: create SecurityCOnfig.

---
+++ src/main/java/com/codecool/employeemanager/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.codecool.employeemanager.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                    .anyRequest().permitAll();
+    }
+}
