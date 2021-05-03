b030ac5583254a5d81c7a78a273e76625e941ef2
Máté Groholy <mate.groholy@gmail.com>
2021-05-03 16:50:30 +0200

    fix: add @Configuration annotation to SecurityCOnfig.

--- src/main/java/com/codecool/employeemanager/security/SecurityConfig.java
+++ src/main/java/com/codecool/employeemanager/security/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.codecool.employeemanager.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
