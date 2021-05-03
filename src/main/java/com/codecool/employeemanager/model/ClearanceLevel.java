004b172c68cda8a04734a4ac9a1328175e9fba60
Kriszti Toth <k95toth@gmail.com>
2021-05-03 18:49:38 +0200

    refactor: remove prefix ROLE_ from ClearanceLevel values

--- src/main/java/com/codecool/employeemanager/model/ClearanceLevel.java
+++ src/main/java/com/codecool/employeemanager/model/ClearanceLevel.java
@@ -3,9 +3,9 @@ package com.codecool.employeemanager.model;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum ClearanceLevel implements GrantedAuthority{
-    ROLE_USER,
-    ROLE_SUPERVISOR,
-    ROLE_ADMIN;
+    USER,
+    SUPERVISOR,
+    ADMIN;
 
     @Override
     public String getAuthority() {
