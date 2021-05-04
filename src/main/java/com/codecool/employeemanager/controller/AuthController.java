ea189a2ff27034e9c1a30ad632396d7ad62f5133
Máté Groholy <mate.groholy@gmail.com>
2021-05-04 13:47:30 +0200

    feat: create and return httpOnly cookie with jwt in AuthController.

--- src/main/java/com/codecool/employeemanager/controller/AuthController.java
+++ src/main/java/com/codecool/employeemanager/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.codecool.employeemanager.controller;
 
+import com.codecool.employeemanager.model.ClearanceLevel;
 import com.codecool.employeemanager.model.UserDto;
+import com.codecool.employeemanager.security.JwtTokenServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,23 +15,35 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @CrossOrigin
 @RestController
 public class AuthController {
 
     private AuthenticationManager authenticationManager;
+    private JwtTokenServices jwtTokenServices;
 
     @Autowired
-    public AuthController(AuthenticationManager authenticationManager) {
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenServices jwtTokenServices) {
         this.authenticationManager = authenticationManager;
+        this.jwtTokenServices = jwtTokenServices;
     }
 
     @PostMapping(path = "/sign-in")
-    public ResponseEntity signIn(@RequestBody UserDto userDto){
+    public ResponseEntity signIn(@RequestBody UserDto userDto, HttpServletResponse httpServletResponse){
         try{
             String email = userDto.getEmail();
             String password = userDto.getPassword();
             Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+            List<ClearanceLevel> levels = authentication.getAuthorities().stream().map(level -> ClearanceLevel.valueOf(level.getAuthority())).collect(Collectors.toList());
+            String jwt = jwtTokenServices.createToken(email, levels);
+            Cookie cookie = new Cookie("jwt", jwt);
+            cookie.setHttpOnly(true);
+            httpServletResponse.addCookie(cookie);
             return ResponseEntity.ok().build();
         } catch (AuthenticationException authenticationException){
             throw new BadCredentialsException("Invalid email/password.");
