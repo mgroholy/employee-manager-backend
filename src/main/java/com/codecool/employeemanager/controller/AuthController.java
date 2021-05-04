b312b7afb75e0991da2b150d878b43675b2f6ed1
Kriszti Toth <k95toth@gmail.com>
2021-05-04 21:14:06 +0200

    feat: send logged in user data

--- src/main/java/com/codecool/employeemanager/controller/AuthController.java
+++ src/main/java/com/codecool/employeemanager/controller/AuthController.java
@@ -11,10 +11,8 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -57,6 +55,12 @@ public class AuthController {
     @PostMapping("/sign-up")
     public void signUp(@RequestBody UserDto userData) {
         userService.addUser(userData);
+    }
+
+    @GetMapping("/user")
+    public String getCurrentUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (String) authentication.getPrincipal();
     }
 
 }
