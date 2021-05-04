e3b22a8ad0d4f0ba5a8376fab294acf40906613f
Máté Groholy <mate.groholy@gmail.com>
2021-05-04 15:56:40 +0200

    feat: add JwtTokenFilter to filterCharin in SecurityConfig. Add base setup.

--- src/main/java/com/codecool/employeemanager/security/SecurityConfig.java
+++ src/main/java/com/codecool/employeemanager/security/SecurityConfig.java
@@ -1,15 +1,24 @@
 package com.codecool.employeemanager.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    @Autowired
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
 
     @Bean
     @Override
@@ -24,7 +33,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                    .authorizeRequests()
-                    .anyRequest().permitAll();
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                    .antMatchers("/sign-in").permitAll()
+                    .antMatchers("/**").authenticated()
+                    .antMatchers("/levels").authenticated()
+                    .antMatchers("/statuses").authenticated()
+                    .antMatchers("/departments").authenticated()
+                    .antMatchers("/employees").authenticated()
+                    .antMatchers("/employees**").authenticated()
+                    .antMatchers("/departments**").authenticated()
+                .anyRequest().denyAll()
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
     }
 }
