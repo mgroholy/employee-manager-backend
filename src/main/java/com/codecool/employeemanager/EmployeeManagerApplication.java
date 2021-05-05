3a368d30986cd57529bfcb0130df51db459c7f8f
Máté Groholy <mate.groholy@gmail.com>
2021-05-05 16:25:09 +0200

    feat: create basic data with Position entity.

--- src/main/java/com/codecool/employeemanager/EmployeeManagerApplication.java
+++ src/main/java/com/codecool/employeemanager/EmployeeManagerApplication.java
@@ -3,6 +3,7 @@ package com.codecool.employeemanager;
 import com.codecool.employeemanager.model.*;
 import com.codecool.employeemanager.repository.DepartmentRepository;
 import com.codecool.employeemanager.repository.EmployeeRepository;
+import com.codecool.employeemanager.repository.PositionRepository;
 import com.codecool.employeemanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -23,13 +24,15 @@ public class EmployeeManagerApplication {
     private EmployeeRepository employeeRepository;
     private DepartmentRepository departmentRepository;
     private UserRepository userRepository;
+    private PositionRepository positionRepository;
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public EmployeeManagerApplication(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository, UserRepository userRepository) {
+    public EmployeeManagerApplication(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository, UserRepository userRepository, PositionRepository positionRepository) {
         this.employeeRepository = employeeRepository;
         this.departmentRepository = departmentRepository;
         this.userRepository = userRepository;
+        this.positionRepository = positionRepository;
         passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
@@ -46,9 +49,14 @@ public class EmployeeManagerApplication {
             Department SALES = Department.builder().name("Sales").build();
             departmentRepository.saveAll(Arrays.asList(IT, HR, SALES));
 
-            Employee john = Employee.builder().name("John Doe").email("john@doe.com").department(IT).phoneNumber("36301234567").dateOfBirth(LocalDate.parse("1970-02-03")).clearanceLevel(ClearanceLevel.USER).position("Developer").status(Status.ACTIVE).dateOfHire(LocalDate.of(2020, 2, 15)).build();
-            Employee jane = Employee.builder().name("Jane Doe").email("jane@doe.com").department(HR).phoneNumber("36305671234").dateOfBirth(LocalDate.parse("1985-10-10")).clearanceLevel(ClearanceLevel.ADMIN).position("Recruiter").status(Status.ACTIVE).dateOfHire(LocalDate.of(2018, 9, 1)).build();
-            Employee jack = Employee.builder().name("Jack Smith").email("jack@smith.com").department(SALES).phoneNumber("36309876543").dateOfBirth(LocalDate.parse("1960-05-25")).clearanceLevel(ClearanceLevel.SUPERVISOR).position("Purchasing leader").status(Status.ACTIVE).dateOfHire(LocalDate.of(2005, 6, 10)).build();
+            Position developer = Position.builder().name("Developer").build();
+            Position recruiter = Position.builder().name("Recruiter").build();
+            Position purchasingLeader = Position.builder().name("Purchasing Leader").build();
+            positionRepository.saveAll(Arrays.asList(developer, recruiter, purchasingLeader));
+
+            Employee john = Employee.builder().name("John Doe").email("john@doe.com").department(IT).phoneNumber("36301234567").dateOfBirth(LocalDate.parse("1970-02-03")).clearanceLevel(ClearanceLevel.USER).position(developer).status(Status.ACTIVE).dateOfHire(LocalDate.of(2020, 2, 15)).build();
+            Employee jane = Employee.builder().name("Jane Doe").email("jane@doe.com").department(HR).phoneNumber("36305671234").dateOfBirth(LocalDate.parse("1985-10-10")).clearanceLevel(ClearanceLevel.ADMIN).position(recruiter).status(Status.ACTIVE).dateOfHire(LocalDate.of(2018, 9, 1)).build();
+            Employee jack = Employee.builder().name("Jack Smith").email("jack@smith.com").department(SALES).phoneNumber("36309876543").dateOfBirth(LocalDate.parse("1960-05-25")).clearanceLevel(ClearanceLevel.SUPERVISOR).position(purchasingLeader).status(Status.ACTIVE).dateOfHire(LocalDate.of(2005, 6, 10)).build();
             employeeRepository.saveAll(Arrays.asList(john, jane, jack));
 
             UserModel jackUser = UserModel.builder().email("jack@smith.com").employee(jack).password(passwordEncoder.encode("password")).levels(List.of(ClearanceLevel.SUPERVISOR)).build();
