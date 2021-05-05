b54ca3e4f63e6f9b3456690c6ed9bed20f20fe08
Máté Groholy <mate.groholy@gmail.com>
2021-05-05 17:05:43 +0200

    feat: add findByName method to PositionRepository.

--- src/main/java/com/codecool/employeemanager/repository/PositionRepository.java
+++ src/main/java/com/codecool/employeemanager/repository/PositionRepository.java
@@ -3,5 +3,9 @@ package com.codecool.employeemanager.repository;
 import com.codecool.employeemanager.model.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PositionRepository extends JpaRepository<Position, Integer> {
+
+    Optional<Position> findByName(String name);
 }
